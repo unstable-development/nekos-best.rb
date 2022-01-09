224896f650884e100857178e53ede84ad3595343
NekoFanatic <83883849+NekoFanatic@users.noreply.github.com>
2022-01-09 20:04:18 +0100

    updated code to prevent api spam

--- lib/nekosbest.rb
+++ lib/nekosbest.rb
@@ -6,7 +6,7 @@ module Nekosbest
 
   require "rubygems"
   require "httparty"
-  
+
   __copyright__ = "Copyright 2021 NekoFanatic"
 
   API_URL = "https://nekos.best/"
@@ -46,69 +46,66 @@ module Nekosbest
 
   class Result
 
-      def initialize(url, artist_href, artist_name, source_url)
-          @url, @artist_href, @artist_name, @source_url = url, artist_href, artist_name, source_url
-      end
+    def initialize(url, artist_href, artist_name, source_url)
+      @url, @artist_href, @artist_name, @source_url = url, artist_href, artist_name, source_url
+    end
 
-      def to_s
-          "<url='#@url', artist_href='#@artist_href', artist_name='#@artist_name', source_url='#@source_url'>"
-      end
+    def to_s
+      "<url='#@url', artist_href='#@artist_href', artist_name='#@artist_name', source_url='#@source_url'>"
+    end
 
-      def url
-          @url
-      end
+    def url
+      @url
+    end
 
-      def artist_href
-          @artist_href
-      end
+    def artist_href
+      @artist_href
+    end
 
-      def artist_name
-          @artist_name
-      end
+    def artist_name
+      @artist_name
+    end
 
-      def source_url
-          @source_url
-      end
+    def source_url
+      @source_url
+    end
   end
 
   class Request
-      include HTTParty
-      base_uri API_URL
 
-      def initialize(type)
-          @type = type
-      end
+    include HTTParty
+    base_uri API_URL
 
-      def posts
-          self.class.get("/api/v1/#@type")
-      end
+    def initialize(type, amount)
+      @type, @amount = type, amount
+    end
+
+    def posts
+      self.class.get("/api/v1/#@type?amount=#@amount")
+    end
   end
 
   def get_img (type, amount = 1)
-      amount = amount.to_i
-      if amount == 0
-          return "Not a valid amount! It has to be a number between 1 and 20"
-      end
-      ar = Array.new
-      if $CATEGORYS.include? type
-          if amount <= 20
-              for a in 1..amount do
-                  g = Request.new(type)
-                  ret = g.posts
-
-                  result = Result.new(ret["url"], ret["artist_href"], ret["artist_name"], ret["source_url"])
-
-                  ar.append(result)
-              end
-              return ar
+    amount = amount.to_i
+    if amount == 0
+      return "Not a valid amount! It has to be a number between 1 and 20"
+    end
+    if $CATEGORYS.include? type
+      if amount <= 20
+        g = Request.new(type, amount)
+        ret = g.posts
+        ar = Array.new
+        for i in ret["url"]
+          ar.append(Result.new(i["url"], i["artist_href"], i["artist_name"], i["source_url"]))
+        end
+        return ar
           
-          else
-              return "The amount has to be a number between 1 and 20 !"
-          end
-      
       else
-          return type + " is not an option! It has to be one ofte following:\n #$CATEGORYS"
+        return "The amount has to be a number between 1 and 20 !"
       end
+      
+    else
+      return type + " is not an option! It has to be one ofte following:\n #$CATEGORYS"
+    end
   end
-
 end
